198de98fd18b0b00cc157e7d02c1efe18873c460
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-01 20:56:14 -0300

    Commit - Building Window

--- mainGUI.py
+++ mainGUI.py
@@ -3,8 +3,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 
 Config.set('graphics', 'resizable', False)
-Config.set('graphics', 'width', '500')
-Config.set('graphics', 'height', '500')
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '150')
 
 class MainGUI(BoxLayout):
     pass
@@ -12,6 +12,7 @@ class MainGUI(BoxLayout):
 
 class Application(App):
     def build(self):
+        self.title = "Image Digital Scanner and Analyzer"
         return MainGUI()
 
 Application().run()
