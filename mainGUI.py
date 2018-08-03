c55bc074eccaa5b859faee1a40323fdc6a8a9d0e
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-03 18:52:33 -0300

    Working

--- mainGUI.py
+++ mainGUI.py
@@ -4,6 +4,7 @@ from kivy.lang import Builder
 from kivy.config import Config
 from kivy.properties import StringProperty
 from CJsonFile import JsonFile
+from COcrReader import OcrReader
 
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '400')
@@ -12,8 +13,9 @@ Config.set('graphics', 'height', '520')
 
 class MainGUI(BoxLayout):
     root_file = JsonFile("root_reg")
-    txt_root_dir = StringProperty('')
+    txt_root_dir = ''
     txt_root_current_dir = StringProperty('')
+    img_ext = StringProperty('')
     data = root_file.json_details_read()
     txt_root_dir = str(data['root'])
     def select_to(self, *args):
@@ -21,6 +23,15 @@ class MainGUI(BoxLayout):
             self.txt_root_dir = str(args[1][0])
             self.root_file.root_persist(self.txt_root_dir)
             self.txt_root_current_dir = str(args[1][0])
+        except:
+            print("Erro")
+
+    def click_run_button(self):
+        root_file = JsonFile("root_reg")
+        data = root_file.json_details_read()
+        ocrreader = OcrReader(data['root'])
+        try:
+            ocrreader.generate_all(self.img_ext)
         except:
             print("Erro")
 
