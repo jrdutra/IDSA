67414fddeb03d8134f2c2262a1947b4a443ac358
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-04 15:50:04 -0300

    ad

--- mainGUI.py
+++ mainGUI.py
@@ -40,8 +40,8 @@ class MainGUI(BoxLayout):
     def click_run_button(self):
         root_file = JsonFile("root_reg")
         data = root_file.json_details_read()
+        root_dir = data['root']
         Clock.schedule_interval(self.gui_refresh, 0.1)
-
         # reset all initial info
 
         self.info_file.json_img_txt_details_conversion(1, 1, "-")
@@ -49,20 +49,28 @@ class MainGUI(BoxLayout):
         self.current_number = 0
         self.rage_numer = 0
         self.current_process_dir = '-'
-        self.info_text = "Stating Conversion"
         try:
-            command = 'start python mainProcess.py ' + data['root'] + ' ' + self.img_ext
+            command = 'start python mainProcess.py ' + str(root_dir) + ' ' + str(self.img_ext)
+            print(command)
             os.system(command)
         except:
             print("Something wrong.")
 
     def gui_refresh(self, dt):
         self.info_data = self.info_file.json_details_read()
+        self.current_number = self.info_data['current']
+        self.rage_numer = self.info_data['range']
+        self.info_data = self.info_file.json_details_read()
         self.percent = self.info_data['%']
         self.current_number = self.info_data['current']
         self.rage_numer = self.info_data['range']
         self.current_process_dir = str(self.info_data['dir'])
-        self.info_text = "File " + str(self.current_number) + " from " + str(self.rage_numer) + " files."
+
+        if self.current_number != 1 and self.rage_numer != 1 and self.rage_numer == self.current_number:
+            self.info_text = "Finished"
+        else:
+            self.info_text = "File " + str(self.current_number) + " from " + str(self.rage_numer) + " files."
+
 
 
 class Application(App):
